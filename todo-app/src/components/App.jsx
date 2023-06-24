115b4ddd253ee0ad6f88d228e445059b98949652
MiMad89 <michal9977@gmail.com>
2023-06-24 23:06:04 +0200

    Update React

---
+++ todo-app/src/components/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export class App extends Component {
+  render () {
+    return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/components/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org/docs/getting-started.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    )
+}
+}
